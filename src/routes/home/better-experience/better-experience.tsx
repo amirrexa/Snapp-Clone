917a5a98794b1ce73743b3d9861f56005ce9efca
amirrexa <amirrezakh123@gmail.com>
2024-04-19 17:14:38 +0330

    🆕 Introducing BetterExperience Component

---
+++ src/routes/home/better-experience/better-experience.tsx
@@ -0,0 +1,11 @@
+import "./better-experience.scss"
+import {Card} from "../../../components/card/card";
+
+export const BetterExperience = () => {
+    return (
+        <Card className={"sc-better-experience"}>
+            <h1>تجربه مدیریت راحت تر با پنل جدید اسنپ!</h1>
+            <p>مدیریت رانندگان و مشتریان، ارتباط با پشتیبانی، ورود و خروج و ...</p>
+        </Card>
+    )
+}
