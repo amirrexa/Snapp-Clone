b60d68d7120d32cf3118afdc72f6070fa19743d0
amirreza <amirrezakh123@gmail.com>
2024-10-20 14:53:02 -0700

    ➕ Added Temporary HRefs

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -9,6 +9,8 @@ export const Home = () => {
             <a href={'http://localhost:3000'} target={'_blank'}>
                 Go To LocalHost
             </a>
+            <a href={"https://pmxv2.viral-team.com/"} target={"_blank"}>Go To PMXV2 Blank</a>
+            <a href={"https://pmxv2.viral-team.com/"} target={"_self"}>Go To PMXV2 Self</a>
             <BetterExperience/>
             <Features/>
             <About />
