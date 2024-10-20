2438286e8e7eced3e72d7be5a9a43c75144ffe8a
amirreza <amirrezakh123@gmail.com>
2024-10-20 15:08:28 -0700

    ➕ Added Temporary HRef

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -6,9 +6,10 @@ import {About} from "./about/about";
 export const Home = () => {
     return (
         <div className={"sc-home"}>
+            <a href={"https://pmxv2.viral-team.com/"} target={"_blank"}>Go To PMXV2 Blank</a>
             <BetterExperience/>
             <Features/>
-            <About />
+            <About/>
         </div>
     )
 }
