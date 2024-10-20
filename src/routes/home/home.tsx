98709ab3918489e7548d1b82772ac80497a5092a
amirreza <amirrezakh123@gmail.com>
2024-10-20 15:28:41 -0700

    ❌ Removed HRef

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -6,7 +6,6 @@ import {About} from "./about/about";
 export const Home = () => {
     return (
         <div className={"sc-home"}>
-            <a href={"https://pmxv2.viral-team.com/"} target={"_blank"}>Go To PMXV2 Blank</a>
             <BetterExperience/>
             <Features/>
             <About/>
