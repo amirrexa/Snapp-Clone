20667694b5b4683567719bbc51cfeb1b1aff5ab6
amirreza <amirrezakh123@gmail.com>
2024-10-20 14:58:07 -0700

    ❌ Removed HRefs

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -6,11 +6,6 @@ import {About} from "./about/about";
 export const Home = () => {
     return (
         <div className={"sc-home"}>
-            <a href={'http://localhost:3000'} target={'_blank'}>
-                Go To LocalHost
-            </a>
-            <a href={"https://pmxv2.viral-team.com/"} target={"_blank"}>Go To PMXV2 Blank</a>
-            <a href={"https://pmxv2.viral-team.com/"} target={"_self"}>Go To PMXV2 Self</a>
             <BetterExperience/>
             <Features/>
             <About />
